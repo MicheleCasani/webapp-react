bef06d4eb4a20cc6fee367c605d1c9be165e91b1
MicheleCasani <casanimichele@gmail.com>
2025-06-03 15:30:50 +0200

    [UPD] add axios

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,55 +1,29 @@
 import React from 'react'
 import FilmCard from '../components/FilmCard'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const FIlmsArray = [
-    {
-        id: 1,
-        title: "Titolo 1",
-        author: "regista 1",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "https://picsum.photos/500/300",
-    },
-    {
-        id: 2,
-        title: "Titolo 2",
-        author: "regista 2",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "https://picsum.photos/500/300",
-    },
-    {
-        id: 3,
-        title: "Titolo 3",
-        author: "regista 3",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "https://picsum.photos/500/300",
-    },
-    {
-        id: 4,
-        title: "Titolo 4",
-        author: "regista 4",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "https://picsum.photos/500/300",
-    },
-    {
-        id: 5,
-        title: "Titolo 5",
-        author: "regista 5",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "https://picsum.photos/500/300",
-    },
-    {
-        id: 6,
-        title: "Titolo 6",
-        author: "regista 6",
-        abstract: "Lorem ipsum dolor sit amet",
-        image: "https://picsum.photos/500/300",
-    },
-];
 
 const HomePage = () => {
 
-    const [films, setFilms] = useState(FIlmsArray);
+    const [films, setFilms] = useState([]);
+
+    const fetchMovies = () => {
+
+        axios
+            .get("http://localhost:3000/movies/")
+            .then((resp) => {
+                setFilms(resp.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [])
 
     return (
         <>
